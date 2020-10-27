c690f30d7704e99b33b529bc3365ae3c07f2f6ea
Dmitry <nemirdota@gmail.com>
2020-10-27 19:24:33 +0200

    Добавлены все необходимые всплывающие подсказки

--- js/main.js
+++ js/main.js
@@ -208,79 +208,105 @@ if ($('.guild__content').length) {
     });
 };
 
-function loadCabinetContent() {
-    $('.background').each(function () {
-        let url = 'url(' + $(this).attr('data-bg-url') + ')'
-        $(this).css('background-image', url);
-    });
-
-    let skinInner = document.getElementById('skin-container');
-    let skinViewer = new skinview3d.FXAASkinViewer(skinInner, {
-        width: 300,
-        height: 400,
-        skin: skinInner.getAttribute('data-skin-url'),
-    });
-    if (skinInner.getAttribute('data-cape-url') != "none") {
-        skinViewer.loadCape(skinInner.getAttribute('data-cape-url'))
+if ($('.user__content').length) {
+    function loadCabinetContent() {
+        $('.background').each(function () {
+            let url = 'url(' + $(this).attr('data-bg-url') + ')'
+            $(this).css('background-image', url);
+        });
+    
+        let skinInner = document.getElementById('skin-container');
+        let skinViewer = new skinview3d.FXAASkinViewer(skinInner, {
+            width: 300,
+            height: 400,
+            skin: skinInner.getAttribute('data-skin-url'),
+        });
+        if (skinInner.getAttribute('data-cape-url') != "none") {
+            skinViewer.loadCape(skinInner.getAttribute('data-cape-url'))
+        };
+        skinViewer.camera.position.x = -15;
+        skinViewer.camera.position.y = 15;
+        let control = skinview3d.createOrbitControls(skinViewer);
+        // Control objects with your mouse!
+        control.enableRotate = true;
+        control.enableZoom = false;
+        control.enablePan = false;
+        // Add an animation
+        let walk = skinViewer.animations.add(skinview3d.WalkingAnimation);
+        // Set the speed of an animation
+        walk.speed = 0.3;
+    
+        $('.tab-link').click(function () {
+            var tab_id = $(this).attr('data-tab');
+    
+            $('.tab-link').removeClass('active');
+            $('.tab').removeClass('active');
+    
+            $(this).addClass('active');
+            $("#" + tab_id).addClass('active');
+        });
+    
+        let online = Number($('.guilds__guild__players').attr('data-guild-now'));
+        let total = Number($('.guilds__guild__players').attr('data-guild-total'));
+        let width = online / total * 100 + '%';
+        $('.guilds__guild__players').find('.guilds__guild__players-now').css('width', width);
     };
-    skinViewer.camera.position.x = -15;
-    skinViewer.camera.position.y = 15;
-    let control = skinview3d.createOrbitControls(skinViewer);
-    // Control objects with your mouse!
-    control.enableRotate = true;
-    control.enableZoom = false;
-    control.enablePan = false;
-    // Add an animation
-    let walk = skinViewer.animations.add(skinview3d.WalkingAnimation);
-    // Set the speed of an animation
-    walk.speed = 0.3;
+    
+    function loadAchievementsContent() {
+        $('.background').each(function () {
+            let url = 'url(' + $(this).attr('data-bg-url') + ')'
+            $(this).css('background-image', url);
+        });
+        $('.user__achievement__progress').each(function () {
+            let progressNow = Number($(this).attr('data-progress-now'));
+            let progressTotal = Number($(this).attr('data-progress-total'));
+            let progressWidth = progressNow / progressTotal * 100 + '%';
+            $(this).find('.user__achievement__progress-now').css('width', progressWidth);
+        });
+        $('.user__achievements a').click(function () {
+            let preloader = $('.user__content').attr('data-preloader');
+            $('.user__content').html('<div class="ajax-preloader-wrap"><div class="ajax-preloader" style="background-image: url(' + "'" + preloader + "'" + ');"></div></div>');
+            $('.user__content').load('user_parts.html #user_achievements_block');
+        });
+    };
+    
+    function loadPrivilegesContent() {
+        $('.user__privilege').each(function () {
+            let privilegeColor = $(this).attr('data-privilege-color');
+            let privilegeTextShadow = '0 0 10px ' + privilegeColor;
+            $(this).css({ color: privilegeColor, outlineColor: privilegeColor, textShadow: privilegeTextShadow });
+        });
+    };
 
-    $('.tab-link').click(function () {
-        var tab_id = $(this).attr('data-tab');
-
-        $('.tab-link').removeClass('active');
-        $('.tab').removeClass('active');
-
-        $(this).addClass('active');
-        $("#" + tab_id).addClass('active');
-    });
-
-    let online = Number($('.guilds__guild__players').attr('data-guild-now'));
-    let total = Number($('.guilds__guild__players').attr('data-guild-total'));
-    let width = online / total * 100 + '%';
-    $('.guilds__guild__players').find('.guilds__guild__players-now').css('width', width);
+    loadCabinetContent();
 };
 
-function loadAchievementsContent() {
-    $('.background').each(function () {
-        let url = 'url(' + $(this).attr('data-bg-url') + ')'
-        $(this).css('background-image', url);
-    });
-    $('.user__achievement__progress').each(function () {
-        let progressNow = Number($(this).attr('data-progress-now'));
-        let progressTotal = Number($(this).attr('data-progress-total'));
-        let progressWidth = progressNow / progressTotal * 100 + '%';
-        $(this).find('.user__achievement__progress-now').css('width', progressWidth);
-    });
-    $('.user__achievements a').click(function () {
-        let preloader = $('.user__content').attr('data-preloader');
-        $('.user__content').html('<div class="ajax-preloader-wrap"><div class="ajax-preloader" style="background-image: url(' + "'" + preloader + "'" + ');"></div></div>');
-        $('.user__content').load('user_parts.html #user_achievements_block');
-    });
+function loadHints() {
+    let hint = document.createElement('div');
+    hint.classList.add('cursor-hint');
+    let showHints = document.querySelectorAll('.show-hint');
+    for (let showHint of showHints) {
+        showHint.onmouseover = function () {
+            document.body.appendChild(hint);
+            hint.innerHTML = this.getAttribute('data-hint');
+            showHint.addEventListener('mousemove', e => {
+                hint.style.top = e.pageY + 3 + 'px';
+                hint.style.left = e.pageX - 3 + 'px';
+                if (hint.offsetWidth + e.pageX > document.body.offsetWidth) {
+                    hint.style.left = e.pageX - (e.pageX + hint.offsetWidth - document.body.offsetWidth) - 3 + 'px';
+                };
+            });
+        };
+        showHint.onmouseout = function () {
+            document.body.removeChild(hint);
+        };
+    };
 };
 
-function loadPrivilegesContent() {
-    $('.user__privilege').each(function() {
-        let privilegeColor = $(this).attr('data-privilege-color');
-        let privilegeTextShadow = '0 0 10px ' + privilegeColor;
-        $(this).css({ color: privilegeColor, outlineColor: privilegeColor, textShadow: privilegeTextShadow });
-    });
-};
+loadHints();
 
 if ($('.user__content').length) {
     let preloader = $('.user__content').attr('data-preloader');
-    $('.user__content').html('<div class="ajax-preloader-wrap"><div class="ajax-preloader" style="background-image: url(' + "'" + preloader + "'" + ');"></div></div>');
-    $('.user__content').load('user_parts.html #user_cabinet', loadCabinetContent);
     $('.user__links a').click(function () {
         if ($(this).attr('data-user-part')) {
             $('.user__links a').removeClass('current');
@@ -290,12 +316,14 @@ if ($('.user__content').length) {
             $('.user__content').load('user_parts.html #' + part, () => {
                 if (part == 'user_cabinet') {
                     loadCabinetContent();
+                    loadHints();
                 } else if (part == 'user_achievements') {
                     loadAchievementsContent();
                 } else if (part == 'user_privileges') {
                     loadPrivilegesContent();
                 } else if (part == 'user_customization') {
                     loadColorLabel();
+                    loadHints();
                 };
             });
         };
